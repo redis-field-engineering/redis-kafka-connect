5ad285b21d0de09d8f5b793f679c6c732cc06ac5
jruaux <jruaux@gmail.com>
2021-05-13 13:29:22 -0700

    added config test

--- src/test/java/com/redislabs/kafkaconnect/RedisEnterpriseSourceConnectorTest.java
+++ src/test/java/com/redislabs/kafkaconnect/RedisEnterpriseSourceConnectorTest.java
@@ -3,8 +3,12 @@ package com.redislabs.kafkaconnect;
 import com.redislabs.kafkaconnect.source.RedisEnterpriseSourceConfig;
 import com.redislabs.kafkaconnect.source.RedisEnterpriseSourceTask;
 import org.apache.kafka.common.config.ConfigDef;
+import org.apache.kafka.common.config.ConfigValue;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RedisEnterpriseSourceConnectorTest {
 
@@ -13,6 +17,11 @@ public class RedisEnterpriseSourceConnectorTest {
         ConfigDef config = new RedisEnterpriseSourceConnector().config();
         Assertions.assertNotNull(config);
         Assertions.assertTrue(config instanceof RedisEnterpriseSourceConfig.RedisEnterpriseSourceConfigDef);
+        Map<String, ConfigValue> results = config.validateAll(new HashMap<>());
+        ConfigValue value = results.get(RedisEnterpriseSourceConfig.STREAM_NAME);
+        Assertions.assertEquals(RedisEnterpriseSourceConfig.STREAM_NAME, value.name());
+        Assertions.assertNull(value.value());
+        Assertions.assertEquals("Invalid value null for configuration redis.stream.name: Missing stream configuration: '" + RedisEnterpriseSourceConfig.STREAM_NAME + "'", value.errorMessages().get(0));
     }
 
     @Test
