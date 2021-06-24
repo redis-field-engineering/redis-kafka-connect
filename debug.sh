16209fbe0ad924aeea3481a815190744eddbd2a6
jruaux <jruaux@gmail.com>
2021-06-24 10:52:19 -0700

    updated documentation link

--- debug.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-: ${SUSPEND:='n'}
-
-set -e
-
-./mvnw clean package
-export KAFKA_DEBUG='y'
-connect-standalone config/connect-avro-docker.properties config/sink-string.properties
