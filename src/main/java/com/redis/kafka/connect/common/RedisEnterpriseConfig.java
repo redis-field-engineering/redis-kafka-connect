581c885d97f7a49d9da21d35da9d7c0d53099ae2
Julien Ruaux <julien@192.168.1.240>
2022-06-27 16:17:12 -0700

    feat: Added username and password config properties. Resolves #13

--- src/main/java/com/redis/kafka/connect/common/RedisEnterpriseConfig.java
+++ src/main/java/com/redis/kafka/connect/common/RedisEnterpriseConfig.java
@@ -15,50 +15,82 @@
  */
 package com.redis.kafka.connect.common;
 
-import com.github.jcustenborder.kafka.connect.utils.config.ConfigKeyBuilder;
-import com.github.jcustenborder.kafka.connect.utils.config.validators.Validators;
-import io.lettuce.core.RedisURI;
+import java.util.Map;
+
 import org.apache.kafka.common.config.AbstractConfig;
 import org.apache.kafka.common.config.ConfigDef;
 
-import java.util.Map;
+import com.github.jcustenborder.kafka.connect.utils.config.ConfigKeyBuilder;
+import com.github.jcustenborder.kafka.connect.utils.config.ConfigUtils;
+import com.github.jcustenborder.kafka.connect.utils.config.validators.Validators;
+
+import io.lettuce.core.RedisURI;
 
 public class RedisEnterpriseConfig extends AbstractConfig {
 
-    public static final String REDIS_URI_CONFIG = "redis.uri";
-    private static final String REDIS_URI_DEFAULT = "redis://localhost:6379";
-    private static final String REDIS_URI_DOC = "URI of the Redis Enterprise database to connect to, e.g. redis://redis-12000.redis.com:12000";
+	public static final String REDIS_URI_CONFIG = "redis.uri";
+	private static final String REDIS_URI_DEFAULT = "redis://localhost:6379";
+	private static final String REDIS_URI_DOC = "URI of the Redis Enterprise database to connect to, e.g. redis://redis-12000.redis.com:12000. For secure connections use rediss URI scheme, e.g. rediss://...";
 
-    public static final String INSECURE_CONFIG = "redis.insecure";
-    public static final String INSECURE_DEFAULT = "false";
-    public static final String INSECURE_DOC = "Allow insecure connections (e.g. invalid certificates) to Redis Enterprise when using SSL.";
+	public static final String INSECURE_CONFIG = "redis.insecure";
+	public static final String INSECURE_DEFAULT = "false";
+	public static final String INSECURE_DOC = "Allow insecure connections (e.g. invalid certificates) to Redis Enterprise when using SSL.";
 
-    public RedisEnterpriseConfig(ConfigDef config, Map<?, ?> originals) {
-        super(config, originals);
-    }
+	public static final String REDIS_USERNAME_CONFIG = "redis.username";
+	private static final String REDIS_USERNAME_DEFAULT = null;
+	private static final String REDIS_USERNAME_DOC = "Username to use to connect to Redis Enterprise database";
 
-    public RedisURI getRedisURI() {
-        RedisURI uri = RedisURI.create(getString(REDIS_URI_CONFIG));
-        uri.setVerifyPeer(!getBoolean(INSECURE_CONFIG));
-        return uri;
-    }
+	public static final String REDIS_PASSWORD_CONFIG = "redis.password";
+	private static final String REDIS_PASSWORD_DEFAULT = null;
+	private static final String REDIS_PASSWORD_DOC = "Password to use to connect to Redis Enterprise database";
 
-    protected static class RedisEnterpriseConfigDef extends ConfigDef {
+	public RedisEnterpriseConfig(ConfigDef config, Map<?, ?> originals) {
+		super(config, originals);
+	}
 
-        protected RedisEnterpriseConfigDef() {
-            defineConfigs();
-        }
+	public RedisURI getRedisURI() {
+		RedisURI uri = RedisURI.create(ConfigUtils.uri(this, REDIS_URI_CONFIG));
+		uri.setVerifyPeer(!getBoolean(INSECURE_CONFIG));
+		String username = getString(REDIS_URI_CONFIG);
+		if (!isEmpty(username)) {
+			uri.setUsername(username);
+		}
+		if (get(REDIS_PASSWORD_CONFIG) != null) {
+			char[] password = ConfigUtils.passwordCharArray(this, REDIS_PASSWORD_CONFIG);
+			if (password.length > 0) {
+				uri.setPassword(password);
+			}
+		}
+		return uri;
+	}
 
-        protected RedisEnterpriseConfigDef(ConfigDef base) {
-            super(base);
-            defineConfigs();
-        }
+	protected static boolean isEmpty(String string) {
+		return string == null || string.isEmpty();
+	}
 
-        private void defineConfigs() {
-            define(ConfigKeyBuilder.of(REDIS_URI_CONFIG, ConfigDef.Type.STRING).documentation(REDIS_URI_DOC).defaultValue(REDIS_URI_DEFAULT).importance(ConfigDef.Importance.HIGH).validator(Validators.validURI("redis", "rediss")).build());
-            define(ConfigKeyBuilder.of(INSECURE_CONFIG, ConfigDef.Type.BOOLEAN).documentation(INSECURE_DOC).defaultValue(INSECURE_DEFAULT).importance(ConfigDef.Importance.MEDIUM).build());
-        }
+	protected static class RedisEnterpriseConfigDef extends ConfigDef {
 
-    }
+		protected RedisEnterpriseConfigDef() {
+			defineConfigs();
+		}
+
+		protected RedisEnterpriseConfigDef(ConfigDef base) {
+			super(base);
+			defineConfigs();
+		}
+
+		private void defineConfigs() {
+			define(ConfigKeyBuilder.of(REDIS_URI_CONFIG, ConfigDef.Type.STRING).documentation(REDIS_URI_DOC)
+					.defaultValue(REDIS_URI_DEFAULT).importance(ConfigDef.Importance.HIGH)
+					.validator(Validators.validURI("redis", "rediss")).build());
+			define(ConfigKeyBuilder.of(REDIS_USERNAME_CONFIG, ConfigDef.Type.STRING).documentation(REDIS_USERNAME_DOC)
+					.defaultValue(REDIS_USERNAME_DEFAULT).importance(ConfigDef.Importance.MEDIUM).build());
+			define(ConfigKeyBuilder.of(REDIS_PASSWORD_CONFIG, ConfigDef.Type.PASSWORD).documentation(REDIS_PASSWORD_DOC)
+					.defaultValue(REDIS_PASSWORD_DEFAULT).importance(ConfigDef.Importance.MEDIUM).build());
+			define(ConfigKeyBuilder.of(INSECURE_CONFIG, ConfigDef.Type.BOOLEAN).documentation(INSECURE_DOC)
+					.defaultValue(INSECURE_DEFAULT).importance(ConfigDef.Importance.MEDIUM).build());
+		}
+
+	}
 
 }
