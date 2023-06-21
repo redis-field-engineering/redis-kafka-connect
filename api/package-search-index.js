8b520bd548f0f40608a4d638d62ab7fa7cbe92bb
runner <runner@fv-az471-189.4fngvl3g3twezghy3dubkofrre.dx.internal.cloudapp.net>
2023-06-21 19:18:49 +0000

    Publish guide for unspecified

---
+++ api/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.redis.kafka.connect"},{"l":"com.redis.kafka.connect.common"},{"l":"com.redis.kafka.connect.sink"},{"l":"com.redis.kafka.connect.source"}]
