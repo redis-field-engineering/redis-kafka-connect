70a7d9b2e9ba1750b64089ace698614f3531dcfd
runner <runner@fv-az570-420.dbl011l0kofevkdmwtujm0njug.bx.internal.cloudapp.net>
2023-12-21 12:00:37 +0000

    Publish guide for unspecified

--- api/package-search-index.js
+++ api/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.redis.kafka.connect"},{"l":"com.redis.kafka.connect.common"},{"l":"com.redis.kafka.connect.sink"},{"l":"com.redis.kafka.connect.source"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.redis.kafka.connect"},{"l":"com.redis.kafka.connect.common"},{"l":"com.redis.kafka.connect.sink"},{"l":"com.redis.kafka.connect.source"}];updateSearchResults();
